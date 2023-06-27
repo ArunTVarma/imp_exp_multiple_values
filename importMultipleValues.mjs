e739eff556294a15c5c8df03a876912b9b846ad4
arun tejaswi varma <atvarma.v@gmail.com>
2023-06-27 04:07:43 +0000

    exp_imp_multiple_values

---
+++ importMultipleValues.mjs
@@ -0,0 +1,6 @@
+import { myArray, bulb, multiplyByFour } from "./exportMultipleValues.mjs";
+
+console.log(myArray);
+console.log(bulb);
+let num_mul_by_4 = multiplyByFour(6);
+console.log(num_mul_by_4);
